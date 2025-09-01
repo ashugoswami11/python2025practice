0fa762488b54b732503d83121b3b19aa6b67127e
Ashutosh Goswami <ashugoswami6737@gmail.com>
2025-09-01 13:26:13 +0530

    initial commit

--- advance_learning/vid_73_Generatorss/prob_3.py
+++ advance_learning/vid_73_Generatorss/prob_3.py
@@ -57,11 +57,8 @@ def even_number(list2):
             yield n
 
 
-result1 = list(number_generator())
-result2 = list(number_square(result1))
-result3 = list(even_number(result2))
-print(result3)
-
+result = even_number(number_square(number_generator()))
+print(list(result))
 
 Ques 4. Temperature Alert System
 Banao ek generator jo random temperatures generate kare (20–40 range).
@@ -76,12 +73,14 @@ def rand_gen():
         else:
             yield rand_val   #in the generators return is not meant to produce an output it usually terminate and exits the generator
 
-print(next(rand_gen()))
+gen = rand_gen()
+for _ in range(10):
+    print(next(gen))
 
 
 Ques 5. Reverse Word by Word
 Ek generator reverse_words(sentence) banao jo ek sentence ko word-by-word reverse karke yield kare.
-👉 Example: "I love Python" → ["I", "evol", "nohtyP"]
+Example: "I love Python" → ["I", "evol", "nohtyP"]
 
 def reverse_word(sentence):
     sentence = sentence.split(" ")
@@ -93,26 +92,66 @@ print(list(reverse_word("i love burger pizza")))
 
 Ques 6. Nth Fibonacci Using Generator
 Banao ek generator jo fibonacci numbers infinite tak generate kare. Phir usme se sirf Nth number pick karke return karo.
-"""
-def fibonacci1(n):
-    a, b = 0, 1
-    list1 = []
-    for _ in range(n):
-        list1.append(a)
-        a, b = b, a + b
-    yield list1[len(list1) - 1]
 
-print(list(fibonacci1(10)))
-
-def fibonacci():
-    a, b = 0, 1
+def fibo_generator():
+    a, b = 0,1
     while True:
         yield a
-        a, b = b, a + b
+        a, b = b, a+b
 
-gen = fibonacci()
-n = 10
-for _ in range(n):
-    num = next(gen)
-print(num)   # Nth Fibonacci
+gen = fibo_generator()
+num = 8
+for _ in range(num):
+    nth = next(gen)
 
+print(nth)
+
+
+Ques 7. Ek generator banao jo file read kare aur
+sirf un lines ko yield kare jisme "ERROR" word ho.
+
+def file_generator(file_name):
+    with open(f"{file_name}", "r" ) as f:
+        f.readline()
+        for line in f:
+            have_error = False
+            for word in line.split():
+                if word == "[ERROR]":
+                    have_error = True
+                    break
+            if have_error :
+                yield line
+
+
+result = list(file_generator("temp.txt"))
+for i in result:
+    print(">>>",i)
+
+
+
+Ques 8. Chunked Prime Stream
+Ek generator banao jo prime numbers generate kare aur unhe chunks me yield kare.
+Example: prime_chunks(10, 3) → [[2, 3, 5], [7, 11, 13], [17, 19, 23], ...]
+
+def prime_chunks(n, chunk_size):
+    x = 0 # increment till x<num
+    i = 2 # starting position of prime number
+    list1 = []
+    while x < n: #  x ki value tab hee increment karna jab koi prime number mil jaaye
+        flag = True
+        for j in range(2, int(i**0.5)+1): #here if don't add 1 then for e.g 2**0.5 = 1.41 after int(2**0.5) it becomes 1 and in range(start,end) start to end-1 so if i add 2 it will run till 1
+            if i % j == 0:
+                flag = False
+                break
+        if flag:
+            list1.append(i)
+            x += 1 # yaha x ki value tab hee increment hogi jab humko ek prime number mil jayega
+        i +=1
+
+    result_list = [list1[k: k+chunk_size]for k in range(0, len(list1), chunk_size)]
+    yield from result_list
+
+
+gen = prime_chunks(10,3)
+print(list(gen))
+"""
